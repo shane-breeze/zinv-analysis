b3d973c4fda5c22b9078b479bdfa72f2269eb2f3
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-09-27 20:06:17 +0100

    Update conda setup

--- setup_conda.sh
+++ setup_conda.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-export PATH="~/miniconda2/bin:$PATH"
-source activate atuproot
 
+# setup conda
+export PATH="~/miniconda2/bin:$PATH"
+source activate py27
+
+# setup root
+source /cvmfs/sft.cern.ch/lcg/contrib/gcc/4.8/x86_64-centos7-gcc48-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.14.04/x86_64-centos7-gcc48-opt/root/bin/thisroot.sh
+
+# setup atuproot
 export PYTHONPATH=$PYTHONPATH:$PWD:$PWD/atuproot
 export TOPDIR=$PWD
