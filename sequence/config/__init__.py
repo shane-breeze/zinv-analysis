cf049caddcdb3f73b5cd93956f4521835c32ea51
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-09-27 20:06:53 +0100

    build sequence takes outdir

--- sequence/config/__init__.py
+++ sequence/config/__init__.py
@@ -1,8 +1,9 @@
 import imp
 from sequence.Readers import ScribblerWrapper
 
-
-def build_sequence(sequence_cfg_path):
+def build_sequence(sequence_cfg_path, outdir):
     seq = imp.load_source('sequence.sequence', sequence_cfg_path)
+    for _, c in seq.sequence:
+        c.outdir = outdir
     return [(ScribblerWrapper(reader), collector)
             for (reader, collector) in seq.sequence]
