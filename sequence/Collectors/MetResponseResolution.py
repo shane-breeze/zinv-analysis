7d3d8bad5d635a066f8327f0ee7125b911c7d183
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-11-25 17:39:03 +0000

    Remove ROOT

--- sequence/Collectors/MetResponseResolution.py
+++ sequence/Collectors/MetResponseResolution.py
@@ -1,6 +1,6 @@
-import ROOT
-ROOT.gROOT.SetBatch(True)
-ROOT.gErrorIgnoreLevel = ROOT.kWarning
+#import ROOT
+#ROOT.gROOT.SetBatch(True)
+#ROOT.gErrorIgnoreLevel = ROOT.kWarning
 
 import os
 import operator
@@ -84,11 +84,11 @@ class MetResponseResolutionCollector(HistCollector):
         df_forfit = df[df.index.get_level_values("process").isin(datasets+["MCSum"])]
 
         # Perform fitting
-        df_fit = df_forfit.groupby(columns_nobin0).apply(fit)
-        df = pd.merge(df.reset_index(["bin0_low", "bin0_upp"]), df_fit,
-                      on=columns_nobin0, how='left', left_index=True)
-        df["formula"] = voigt((df["bin0_low"]+df["bin0_upp"])/2,
-                              df["mean"], df["sigma"], df["gamma"])
+        #df_fit = df_forfit.groupby(columns_nobin0).apply(fit)
+        #df = pd.merge(df.reset_index(["bin0_low", "bin0_upp"]), df_fit,
+        #              on=columns_nobin0, how='left', left_index=True)
+        #df["formula"] = voigt((df["bin0_low"]+df["bin0_upp"])/2,
+        #                      df["mean"], df["sigma"], df["gamma"])
         df = df.set_index(["bin0_low", "bin0_upp"], append=True)
 
         # Draw 1D plots
@@ -178,139 +178,140 @@ class MetResponseResolutionCollector(HistCollector):
                 pickle.dump((df_group, bins, filepath, cfg), f)
             #args.append((dist_ratio, (df_group, bins, filepath, cfg)))
 
-        df_fit = df_fit[~(
-            df_fit.index.get_level_values("process").isin(datasets)\
-            & (df_fit.index.get_level_values("process")\
-               != df_fit.index.get_level_values("dataset")))]
+        #df_fit = df_fit[~(
+        #    df_fit.index.get_level_values("process").isin(datasets)\
+        #    & (df_fit.index.get_level_values("process")\
+        #       != df_fit.index.get_level_values("dataset")))]
 
-        for cat, df_group in df_fit.groupby(columns_noproc_nobin0_novar_nobin1):
-            df_grp = df_group[["mean", "mean_unc", "width", "width_unc"]]
-            df_nominal = df_grp[df_grp.index.get_level_values("variation").isin(["nominal"])]\
-                    .reset_index("variation", drop=True)
-            for variation in self.variations:
-                df_var = df_grp[df_grp.index.get_level_values("variation").isin([variation])]\
-                        .reset_index("variation", drop=True)
-                df_var = np.abs(df_var - df_nominal)
-                for attr in ["mean", "width"]:
-                    df_nominal["{}_unc_{}".format(attr, variation)] = df_var[attr]
+        #for cat, df_group in df_fit.groupby(columns_noproc_nobin0_novar_nobin1):
+        #    df_grp = df_group[["mean", "mean_unc", "width", "width_unc"]]
+        #    df_nominal = df_grp[df_grp.index.get_level_values("variation").isin(["nominal"])]\
+        #            .reset_index("variation", drop=True)
+        #    for variation in self.variations:
+        #        df_var = df_grp[df_grp.index.get_level_values("variation").isin([variation])]\
+        #                .reset_index("variation", drop=True)
+        #        df_var = np.abs(df_var - df_nominal)
+        #        for attr in ["mean", "width"]:
+        #            df_nominal["{}_unc_{}".format(attr, variation)] = df_var[attr]
 
-            path = os.path.join(self.outdir, "plots", *cat[:2])
-            if not os.path.exists(path):
-                os.makedirs(path)
+        #    path = os.path.join(self.outdir, "plots", *cat[:2])
+        #    if not os.path.exists(path):
+        #        os.makedirs(path)
 
-            for label, attr in [("\mu", "mean"), ("\sigma", "width")]:
-                filename = "{}__{}".format(cat[3], attr)
-                filepath = os.path.abspath(os.path.join(path, filename))
-                df_toplot = df_nominal[[c for c in df_nominal.columns if attr in c]]
-                cfg = copy.deepcopy(self.cfg)
+        #    for label, attr in [("\mu", "mean"), ("\sigma", "width")]:
+        #        filename = "{}__{}".format(cat[3], attr)
+        #        filepath = os.path.abspath(os.path.join(path, filename))
+        #        df_toplot = df_nominal[[c for c in df_nominal.columns if attr in c]]
+        #        cfg = copy.deepcopy(self.cfg)
 
-                xlabel = self.cfg.axis_label.get(cat[3].split("__")[1], cat[3].split("__")[1])
-                ylabel = self.cfg.axis_label.get(cat[3].split("__")[0], cat[3].split("__")[1])
-                match = latex_eq_regex.search(ylabel)
-                if match: ylabel = r'${}({})$'.format(label, match.group(0).replace("$", ""))
-                cfg.xlabel = xlabel
-                cfg.ylabel = ylabel
+        #        xlabel = self.cfg.axis_label.get(cat[3].split("__")[1], cat[3].split("__")[1])
+        #        ylabel = self.cfg.axis_label.get(cat[3].split("__")[0], cat[3].split("__")[1])
+        #        match = latex_eq_regex.search(ylabel)
+        #        if match: ylabel = r'${}({})$'.format(label, match.group(0).replace("$", ""))
+        #        cfg.xlabel = xlabel
+        #        cfg.ylabel = ylabel
 
-                #dist_scatter_pull(df_toplot, self.variations, filepath, cfg)
-                bins = binning[cat[3]][0] # 2D -> 1D profile
-                with open(filepath+".pkl", 'w') as f:
-                    pickle.dump((df_toplot, bins, self.variations, filepath, cfg), f)
-                args.append((dist_scatter_pull, (df_toplot, bins, self.variations, filepath, cfg)))
+        #        #dist_scatter_pull(df_toplot, self.variations, filepath, cfg)
+        #        bins = binning[cat[3]][0] # 2D -> 1D profile
+        #        with open(filepath+".pkl", 'w') as f:
+        #            pickle.dump((df_toplot, bins, self.variations, filepath, cfg), f)
+        #        args.append((dist_scatter_pull, (df_toplot, bins, self.variations, filepath, cfg)))
 
         return args
 
 def fit(df):
-    bins = df.index.get_level_values("bin0_low").values[1:]
-    yields = df[["yield"]].values[1:-1].ravel()
-    vars = df[["variance"]].values[1:-1].ravel()
-
-    if yields.sum() == 0.:
-        return pd.DataFrame({
-            "mean": [0.],
-            "mean_unc": [0.],
-            "sigma": [0.],
-            "sigma_unc": [0.],
-            "gamma": [0.],
-            "gamma_unc": [0.],
-            "width": [0.],
-            "width_unc": [0.],
-            "chi2": [0.],
-            "ndof": [0.],
-        })
-
-    hdata = ROOT.TH1D("data", "", len(bins)-1, bins)
-
-    errs = np.array_equal(yields, vars)
-    if errs: hdata.Sumw2()
-
-    for ibin in range(1, hdata.GetNbinsX()+1):
-        hdata.SetBinContent(ibin, yields[ibin-1])
-        hdata.SetBinError(ibin, np.sqrt(vars[ibin-1]))
-
-    x = ROOT.RooRealVar("x", "x", bins[0], bins[-1])
-    #xframe = x.frame()
-    l = ROOT.RooArgList(x)
-    data = ROOT.RooDataHist("data", "data", l, hdata)
-
-    mean_guess = hdata.GetMean()
-    width_guess = hdata.GetStdDev()
-
-    mu_eq = "mu[{},{},{}]".format(mean_guess, bins[0], bins[-1])
-    gam_eq = "gam[{},{},{}]".format(width_guess, width_guess/20., width_guess*20.)
-    sig_eq = "sig[{},{},{}]".format(width_guess, width_guess/20., width_guess*20.)
-    voig_eq = "Voigtian:voig(x, {}, {}, {})".format(mu_eq, gam_eq, sig_eq)
-
-    ws = ROOT.RooWorkspace("ws")
-    getattr(ws, "import")(x)
-    ws.factory(voig_eq)
-    model = ws.pdf("voig")
-
-    args = [ROOT.RooFit.Minimizer("Minuit2", "Migrad"),
-            ROOT.RooFit.Offset(True),
-            ROOT.RooFit.PrintLevel(-1),
-            ROOT.RooFit.Save()]
-    if errs:
-        args.append(ROOT.RooFit.SumW2Error(True))
-
-    fit_result = model.fitTo(data, *args)
-    #data.plotOn(xframe)
-    #model.plotOn(xframe)
-    #chi2 = xframe.chiSquare(3)
-    chi2 = 0.
-
-    mu = ws.var("mu")
-    mean = ufloat(mu.getValV(), mu.getError())
-
-    sig = ws.var("sig")
-    sigma = ufloat(sig.getValV(), sig.getError())
-
-    gam = ws.var("gam")
-    gamma = ufloat(gam.getValV(), gam.getError())
-
-    # https://en.wikipedia.org/wiki/Voigt_profile
-    # FWHM / (2*sqrt(2*ln(2)))
-    width_breit = 2.*gamma
-    width_gauss = 2.*sigma*(2.*np.log(2.)**0.5)
-    if width_gauss != 0.:
-        phi = width_breit / width_gauss
-        c0 = 2.0056
-        c1 = 1.0593
-        width = sigma * (1. - c0*c1 + (phi**2 + 2.*c1*phi + c0**2*c1**2)**0.5)
-    else:
-        width = width_breit
-
-    return pd.DataFrame({
-        "mean": [mean.n],
-        "mean_unc": [mean.s],
-        "sigma": [sigma.n],
-        "sigma_unc": [sigma.s],
-        "gamma": [gamma.n],
-        "gamma_unc": [gamma.s],
-        "width": [width.n],
-        "width_unc": [width.s],
-        "chi2": [chi2],
-        "ndof": [len(bins)-3],
-    })
+    return None
+#    bins = df.index.get_level_values("bin0_low").values[1:]
+#    yields = df[["yield"]].values[1:-1].ravel()
+#    vars = df[["variance"]].values[1:-1].ravel()
+#
+#    if yields.sum() == 0.:
+#        return pd.DataFrame({
+#            "mean": [0.],
+#            "mean_unc": [0.],
+#            "sigma": [0.],
+#            "sigma_unc": [0.],
+#            "gamma": [0.],
+#            "gamma_unc": [0.],
+#            "width": [0.],
+#            "width_unc": [0.],
+#            "chi2": [0.],
+#            "ndof": [0.],
+#        })
+#
+#    hdata = ROOT.TH1D("data", "", len(bins)-1, bins)
+#
+#    errs = np.array_equal(yields, vars)
+#    if errs: hdata.Sumw2()
+#
+#    for ibin in range(1, hdata.GetNbinsX()+1):
+#        hdata.SetBinContent(ibin, yields[ibin-1])
+#        hdata.SetBinError(ibin, np.sqrt(vars[ibin-1]))
+#
+#    x = ROOT.RooRealVar("x", "x", bins[0], bins[-1])
+#    #xframe = x.frame()
+#    l = ROOT.RooArgList(x)
+#    data = ROOT.RooDataHist("data", "data", l, hdata)
+#
+#    mean_guess = hdata.GetMean()
+#    width_guess = hdata.GetStdDev()
+#
+#    mu_eq = "mu[{},{},{}]".format(mean_guess, bins[0], bins[-1])
+#    gam_eq = "gam[{},{},{}]".format(width_guess, width_guess/20., width_guess*20.)
+#    sig_eq = "sig[{},{},{}]".format(width_guess, width_guess/20., width_guess*20.)
+#    voig_eq = "Voigtian:voig(x, {}, {}, {})".format(mu_eq, gam_eq, sig_eq)
+#
+#    ws = ROOT.RooWorkspace("ws")
+#    getattr(ws, "import")(x)
+#    ws.factory(voig_eq)
+#    model = ws.pdf("voig")
+#
+#    args = [ROOT.RooFit.Minimizer("Minuit2", "Migrad"),
+#            ROOT.RooFit.Offset(True),
+#            ROOT.RooFit.PrintLevel(-1),
+#            ROOT.RooFit.Save()]
+#    if errs:
+#        args.append(ROOT.RooFit.SumW2Error(True))
+#
+#    fit_result = model.fitTo(data, *args)
+#    #data.plotOn(xframe)
+#    #model.plotOn(xframe)
+#    #chi2 = xframe.chiSquare(3)
+#    chi2 = 0.
+#
+#    mu = ws.var("mu")
+#    mean = ufloat(mu.getValV(), mu.getError())
+#
+#    sig = ws.var("sig")
+#    sigma = ufloat(sig.getValV(), sig.getError())
+#
+#    gam = ws.var("gam")
+#    gamma = ufloat(gam.getValV(), gam.getError())
+#
+#    # https://en.wikipedia.org/wiki/Voigt_profile
+#    # FWHM / (2*sqrt(2*ln(2)))
+#    width_breit = 2.*gamma
+#    width_gauss = 2.*sigma*(2.*np.log(2.)**0.5)
+#    if width_gauss != 0.:
+#        phi = width_breit / width_gauss
+#        c0 = 2.0056
+#        c1 = 1.0593
+#        width = sigma * (1. - c0*c1 + (phi**2 + 2.*c1*phi + c0**2*c1**2)**0.5)
+#    else:
+#        width = width_breit
+#
+#    return pd.DataFrame({
+#        "mean": [mean.n],
+#        "mean_unc": [mean.s],
+#        "sigma": [sigma.n],
+#        "sigma_unc": [sigma.s],
+#        "gamma": [gamma.n],
+#        "gamma_unc": [gamma.s],
+#        "width": [width.n],
+#        "width_unc": [width.s],
+#        "chi2": [chi2],
+#        "ndof": [len(bins)-3],
+#    })
 
 def voigt(x, mean, sigma, gamma):
     z = ((x-mean) + 1j*gamma*0.5) / (sigma*np.sqrt(2))
