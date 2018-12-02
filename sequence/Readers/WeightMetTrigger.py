28972c8be663bc7f4ba2c7789cae06bef274c223
Shane Breeze <s.breeze15@imperial.ac.uk>
2018-12-02 15:47:53 +0000

    Update MET trigger SF

--- sequence/Readers/WeightMetTrigger.py
+++ sequence/Readers/WeightMetTrigger.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import erf
 from numba import njit, float32
 
 class WeightMetTrigger(object):
@@ -9,51 +10,78 @@ class WeightMetTrigger(object):
         self.cats = sorted([nmu for nmu in self.correction_files.keys()])
         self.bins = []
         self.corr = []
-        self.corr_up = []
-        self.corr_down = []
+        self.stat_up = []
+        self.stat_down = []
+        self.syst_up = []
+        self.syst_down = []
+        self.eff = {}
         for nmuon in self.cats:
-            bins, corr, corr_up, corr_down = read_file(self.correction_files[nmuon])
+            bins, corr, stat_up, stat_down, syst_up, syst_down = read_file(self.correction_files[nmuon])
             self.bins.append(bins)
             self.corr.append(corr)
-            self.corr_up.append(corr_up)
-            self.corr_down.append(corr_down)
+            self.stat_up.append(stat_up)
+            self.stat_down.append(stat_down)
+            self.syst_up.append(syst_up)
+            self.syst_down.append(syst_down)
 
     def event(self, event):
         nmuons = event.MuonSelection.stops - event.MuonSelection.starts
         met = event.METnoX.pt
-        corrs, corrs_up, corrs_down = get_correction(
-            self.cats, self.bins, self.corr, self.corr_up, self.corr_down,
-            nmuons, met,
+        corrs, stats_up, stats_down, systs_up, systs_down = get_correction(
+            self.cats, self.bins, self.corr, self.stat_up, self.stat_down,
+            self.syst_up, self.syst_down, nmuons, met,
         )
         event.Weight_MET *= corrs
-        event.Weight_metTrigSFUp = np.divide(corrs_up, corrs,
-                                             out=np.ones_like(corrs_up),
-                                             where=corrs!=0)
-        event.Weight_metTrigSFDown = np.divide(corrs_down, corrs,
-                                               out=np.ones_like(corrs_down),
+        event.Weight_metTrigStatUp = np.divide(stats_up, corrs,
+                                               out=np.ones_like(stats_up),
                                                where=corrs!=0)
+        event.Weight_metTrigStatDown = np.divide(stats_down, corrs,
+                                                 out=np.ones_like(stats_down),
+                                                 where=corrs!=0)
+        event.Weight_metTrigSystUp = np.divide(systs_up, corrs,
+                                               out=np.ones_like(systs_up),
+                                               where=corrs!=0)
+        event.Weight_metTrigSystDown = np.divide(systs_down, corrs,
+                                                 out=np.ones_like(systs_down),
+                                                 where=corrs!=0)
 
 @njit
-def get_correction(cats, bins, incorr, incorr_up, incorr_down, nmuons, met):
+def get_correction(cats, bins, incorr, instat_up, instat_down, insyst_up, insyst_down, nmuons, met):
     nev = nmuons.shape[0]
     corrs = np.ones(nev, dtype=float32)
-    corrs_up = np.ones(nev, dtype=float32)
-    corrs_down = np.ones(nev, dtype=float32)
+    stats_up = np.ones(nev, dtype=float32)
+    stats_down = np.ones(nev, dtype=float32)
+    systs_up = np.ones(nev, dtype=float32)
+    systs_down = np.ones(nev, dtype=float32)
 
     for iev in range(nev):
         if nmuons[iev] not in cats:
             continue
 
         cat = cats.index(nmuons[iev])
-        for ibin in range(bins[cat].shape[0]):
-            if bins[cat][ibin,0] <= met[iev] < bins[cat][ibin,1]:
-                corrs[iev] = incorr[cat][ibin]
-                corrs_up[iev] = incorr_up[cat][ibin]
-                corrs_down[iev] = incorr_down[cat][ibin]
-                break
-    return corrs, corrs_up, corrs_down
+        xvals = (bins[cat][:,0]+bins[cat][:,1])/2
+        corrs[iev] = interp(met[iev], xvals, incorr[cat])
+        stats_up[iev] = interp(met[iev], xvals, instat_up[cat])
+        stats_down[iev] = interp(met[iev], xvals, instat_down[cat])
+        systs_up[iev] = interp(met[iev], xvals, insyst_up[cat])
+        systs_down[iev] = interp(met[iev], xvals, insyst_down[cat])
+    return corrs, stats_up, stats_down, systs_up, systs_down
 
-def read_file(path, overflow=True):
+@njit
+def interp(x, xp, fp):
+    nx = xp.shape[0]
+
+    if x < xp[0]:
+        return fp[0]
+    elif x > xp[-1]:
+        return fp[-1]
+
+    for ix in range(nx-1):
+        if xp[ix] <= x < xp[ix+1]:
+            return (x - xp[ix]) * (fp[ix+1] - fp[ix]) / (xp[ix+1] - xp[ix]) + fp[ix]
+    return np.nan
+
+def read_file(path):
     with open(path, 'r') as f:
         lines = [l.split()
                  for l in f.read().splitlines()
@@ -61,10 +89,12 @@ def read_file(path, overflow=True):
 
     bins = np.array([map(float, l[1:3]) for l in lines])
     corr = np.array([float(l[3]) for l in lines])
-    corr_up = corr + np.array([float(l[5]) for l in lines])
-    corr_down = corr - np.array([float(l[4]) for l in lines])
+    stat_up = corr + np.array([float(l[5]) for l in lines])
+    stat_down = corr - np.array([float(l[4]) for l in lines])
+    syst_up = corr + np.array([float(l[7]) for l in lines])
+    syst_down = corr + np.array([float(l[6]) for l in lines])
 
-    if overflow:
-        bins[-1,-1] = np.infty
+    #if overflow:
+    #    bins[-1,-1] = np.infty
 
-    return bins, corr, corr_up, corr_down
+    return bins, corr, stat_up, stat_down, syst_up, syst_down
