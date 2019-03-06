907d3f11540f94a1c5507caf8cea4f49446b0b93
Shane Breeze <s.breeze15@imperial.ac.uk>
2019-03-06 20:10:39 +0000

    Fix event tools tests

--- tests/unit/sequence/Readers/test_EventTools.py
+++ tests/unit/sequence/Readers/test_EventTools.py
@@ -1,11 +1,12 @@
 import pytest
 import mock
-from cachetools import LRUCache
+import numpy as np
 
 from sequence.Readers import EventTools
 
 class DummyEvent(object):
     def __init__(self):
+        self.MET_pt = np.array([1., 2.])
         self._callable_cache = {}
 
 @pytest.fixture()
