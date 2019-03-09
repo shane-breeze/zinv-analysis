0b986381d5265f76aeea02a4f5672f82e3132ad3
Shane Breeze <s.breeze15@imperial.ac.uk>
2019-03-09 23:07:45 +0000

    Speed up histogramming

--- utils/NumbaFuncs.py
+++ utils/NumbaFuncs.py
@@ -1,53 +1,74 @@
 import numpy as np
-from numba import njit, boolean, int32, float32, float64
+import numba as nb
 
-@njit
 def get_bin_indices(vars, mins, maxs, size):
-    result = -1*np.ones((vars[0].shape[0], size), dtype=int32)
-    for iev in range(vars[0].shape[0]):
-        pos = 0
-        for ib in range(mins[0].shape[0]):
-            accept = True
-            for idx in range(len(vars)):
-                if not (mins[idx][ib] <= vars[idx][iev] < maxs[idx][ib]):
-                    accept = False
+    @nb.njit
+    def get_multibin_indices(vars_, mins_, maxs_, size_):
+        result = -1*np.ones((vars_[0].shape[0], size_), dtype=nb.int32)
+        for iev in range(vars_[0].shape[0]):
+            pos = 0
+            for ib in range(mins_[0].shape[0]):
+                accept = True
+                for idx in range(len(vars_)):
+                    if not (mins_[idx][ib] <= vars_[idx][iev] < maxs_[idx][ib]):
+                        accept = False
 
-            if accept:
-                assert pos < size
-                result[iev,pos] = ib
-                pos += 1
+                if accept:
+                    assert pos < size_
+                    result[iev,pos] = ib
+                    pos += 1
 
-        #assert pos == size
+            #assert pos == size_
 
-    return result
+        return result
 
-@njit
+    @nb.njit
+    def get_1dbin_indices(var_, min_, max_, size_):
+        result = -1*np.ones((var_.shape[0], size_), dtype=nb.int32)
+        for iev in range(var_.shape[0]):
+            pos = 0
+            for ib in range(min_.shape[0]):
+                if min_[ib] <= var_[iev] < max_[ib]:
+                    assert pos < size_
+                    result[iev,pos] = ib
+                    pos += 1
+
+            # assert pos == size_
+
+        return result
+
+    if len(vars) == 1:
+        return get_1dbin_indices(vars[0], mins[0], maxs[0], size)
+    else:
+        return get_multibin_indices(vars, mins, maxs, size)
+
+@nb.njit
 def get_nth_sorted_object_indices(n, pts, starts, stops):
-    idxs = -1*np.ones_like(starts, dtype=int32)
+    idxs = -1*np.ones_like(starts, dtype=nb.int32)
     for iev, (start, stop) in enumerate(zip(starts, stops)):
         if n < stop-start:
             idxs[iev] = start + np.argsort(pts[start:stop])[::-1][n]
     return idxs
 
-@njit
+@nb.njit
 def get_event_object_idx(contents, starts, stops):
-    evidx = -1*np.ones_like(contents, dtype=int32)
-    obidx = -1*np.ones_like(contents, dtype=int32)
+    evidx = -1*np.ones_like(contents, dtype=nb.int32)
+    obidx = -1*np.ones_like(contents, dtype=nb.int32)
     for idx, (start, stop) in enumerate(zip(starts, stops)):
         evidx[start:stop] = idx
         for subidx in range(start, stop):
             obidx[subidx] = subidx-start
     return evidx, obidx
 
-@njit
+@nb.njit
 def event_to_object_var(variable, starts, stops):
-    new_obj_var = np.zeros(stops[-1], dtype=float32)
+    new_obj_var = np.zeros(stops[-1], dtype=nb.float32)
     for idx, (start, stop) in enumerate(zip(starts, stops)):
         for subidx in range(start, stop):
             new_obj_var[subidx] = variable[idx]
     return new_obj_var
 
-@njit
+@nb.njit
 def interp(x, xp, fp):
     if x < xp[0]:
         return fp[0]
@@ -59,13 +80,45 @@ def interp(x, xp, fp):
             return (x - xp[ix]) * (fp[ix+1] - fp[ix]) / (xp[ix+1] - xp[ix]) + fp[ix]
     return np.nan
 
-@njit
+@nb.njit
 def interpolate(x, xp, fp):
-    result = np.zeros_like(x, dtype=float64)
+    result = np.zeros_like(x, dtype=np.float64)
     for idx in range(x.shape[0]):
         result[idx] = interp(x[idx], xp[idx,:], fp[idx,:])
     return result
 
-@njit
+@nb.njit
 def weight_numba(nominal, nsig, up, down):
     return nominal * (1 + (nsig>=0)*nsig*up - (nsig<0)*nsig*down)
+
+# This is slow for some reason!?
+@nb.njit
+def histogramdd_numba(event_attrs, mins, maxs, weights):
+    ndim = len(event_attrs)
+    nev = event_attrs[0].shape[0]
+    nib = mins[0].shape[0]
+    hist = np.zeros(nib, dtype=nb.float32)
+
+    for iev in range(nev):
+        for ib in range(nib):
+            accept = True
+            for idx in range(ndim):
+                if not (mins[idx][ib] <= event_attrs[idx][iev] < maxs[idx][ib]):
+                    accept = False
+
+            if accept:
+                hist[ib] += weights[iev]
+
+    return hist
+
+@nb.njit
+def histogram1d_numba(attr, min_, max_, weights):
+    hist = np.zeros(min_.shape[0], dtype=nb.float32)
+
+    for iev, x in enumerate(attr):
+        for ib, (mn, mx) in enumerate(zip(min_, max_)):
+            if mn <= x < mx:
+                hist[ib] += weights[iev]
+                break
+
+    return hist
