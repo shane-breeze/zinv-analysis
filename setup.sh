8fc419f8f85dc1f92537decd20adad51c884cdb5
Shane Breeze <s.breeze15@imperial.ac.uk>
2019-03-10 12:53:57 +0000

    Update pip frozen packages and setup

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 python setup.py install
 
 # setup root
-source /cvmfs/sft.cern.ch/lcg/contrib/gcc/4.8/x86_64-centos7-gcc48-opt/setup.sh
-source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.14.04/x86_64-centos7-gcc48-opt/root/bin/thisroot.sh
+#source /cvmfs/sft.cern.ch/lcg/contrib/gcc/4.8/x86_64-centos7-gcc48-opt/setup.sh
+#source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.14.04/x86_64-centos7-gcc48-opt/root/bin/thisroot.sh
 
 # fix bug with malloc
-export LD_PRELOAD="/usr/lib64/libtcmalloc.so.4"
+#export LD_PRELOAD="/usr/lib64/libtcmalloc.so.4"
 
 # setup atuproot
 export PYTHONPATH=$PYTHONPATH:$PWD
