b805038e4db673e31f1a0447d2262ce5558d6b81
Shane Breeze <s.breeze15@imperial.ac.uk>
2019-03-12 19:15:43 +0000

    Fix bugs in JecVariations

--- tests/unit/sequence/Readers/test_JecVariations.py
+++ tests/unit/sequence/Readers/test_JecVariations.py
@@ -31,60 +31,141 @@ def module():
         unclust_threshold = 15.,
     )
 
-#def test_jec_variations_begin(module, event):
-#    module.begin(event)
-#    assert all(t in ["Total", "AbsoluteStat"] for t in module.jes_sources)
-#    assert all(t in ["Total", "AbsoluteStat"] for t in event.JetSources)
+def test_jec_variations_begin(module, event):
+    module.begin(event)
+    assert all(t in ["Total", "AbsoluteStat"] for t in module.jes_sources)
+    assert all(t in ["Total", "AbsoluteStat"] for t in event.JetSources)
 
-#def test_jec_variations_event_empty(module, event):
-#    jet_pt = awk.JaggedArray(
-#        np.array([], dtype=np.int32),
-#        np.array([], dtype=np.int32),
-#        np.array([], dtype=np.float32),
-#    )
-#    jet_eta = awk.JaggedArray(
-#        np.array([], dtype=np.int32),
-#        np.array([], dtype=np.int32),
-#        np.array([], dtype=np.float32),
-#    )
-#    rho = np.array([], dtype=np.float32)
-#
-#    jet_genjetidx = awk.JaggedArray(
-#        np.array([], dtype=np.int32),
-#        np.array([], dtype=np.int32),
-#        np.array([], dtype=np.int32),
-#    )
-#    genjet_pt = awk.JaggedArray(
-#        np.array([], dtype=np.int32),
-#        np.array([], dtype=np.int32),
-#        np.array([], dtype=np.float32),
-#    )
-#    met_pt = np.array([], dtype=np.float32)
-#    met_phi = np.array([], dtype=np.float32)
-#    def jet_ptshift(self):
-#        return awk.JaggedArray(
-#            np.array([], dtype=np.int32),
-#            np.array([], dtype=np.int32),
-#            np.array([], dtype=np.float32),
-#        )
-#    jet_phi = awk.JaggedArray(
-#        np.array([], dtype=np.int32),
-#        np.array([], dtype=np.int32),
-#        np.array([], dtype=np.float32),
-#    )
-#
-#    event.Jet_pt = jet_pt
-#    event.Jet_eta = jet_eta
-#    event.fixedGridRhoFastjetAll = rho
-#
-#    event.Jet_genJetIdx = jet_genjetidx
-#    event.GenJet_pt = genjet_pt
-#    event.MET_pt = met_pt
-#    event.MET_phi = met_phi
-#    event.Jet_ptShift = mock.Mock(side_effect=jet_ptshift)
-#    event.Jet_phi = jet_phi
-#
-#    module.begin(event)
-#    module.event(event)
-#
-#    assert np.array_equal(event.Jet_ptResolution, awk.JaggedArray([], [], []))
+@pytest.mark.parametrize(
+    "starts,stopys,jpt,jeta,jphi,jgjidx,gjpt,rho,met,mephi,jptres,jjersf,jjersfdown,jjersfup,new_met,new_mephi", [(
+        [0, 0, 1],
+        [0, 1, 3],
+        [16., 60., 70.],
+        [-3.1, 0.5, 3.1],
+        [0.1, 0.3, 0.5],
+        [0, -1, 1],
+        [55., 65., 75.],
+        [0., 20., 50.],
+        [200., 220., 240.],
+        [0.9, 0.1, -0.7],
+        [0.456227396877664, 0.131449148093119, 0.179063214239771],
+        [0.200500000000000, 1.271589764166470, 0.976571428571429],
+        [1.519638403990030, -0.061708613189990, 0.009142773551785],
+        [-1., 0.050076915810078, -0.009142773551785],
+        [200., 236., 232.10981279053],
+        [0.9, 0.1, -0.75251459154],
+    )],
+)
+def test_jec_variations_event(
+    module, event,
+    starts, stopys, jpt, jeta, jphi, jgjidx, gjpt, rho, met, mephi,
+    jptres, jjersf, jjersfdown, jjersfup, new_met, new_mephi,
+):
+    def norm(*args, **kwargs):
+        return 0.5
+    np.random.normal = mock.Mock(side_effect=norm)
+
+    jet_pt = awk.JaggedArray(
+        np.array(starts, dtype=np.int32),
+        np.array(stopys, dtype=np.int32),
+        np.array(jpt, dtype=np.float32),
+    )
+    jet_eta = awk.JaggedArray(
+        np.array(starts, dtype=np.int32),
+        np.array(stopys, dtype=np.int32),
+        np.array(jeta, dtype=np.float32),
+    )
+    rho = np.array(rho, dtype=np.float32)
+
+    jet_genjetidx = awk.JaggedArray(
+        np.array(starts, dtype=np.int32),
+        np.array(stopys, dtype=np.int32),
+        np.array(jgjidx, dtype=np.int32),
+    )
+    genjet_pt = awk.JaggedArray(
+        np.array(starts, dtype=np.int32),
+        np.array(stopys, dtype=np.int32),
+        np.array(gjpt, dtype=np.float32),
+    )
+    met_pt = np.array(met, dtype=np.float32)
+    met_phi = np.array(mephi, dtype=np.float32)
+    jet_phi = awk.JaggedArray(
+        np.array(starts, dtype=np.int32),
+        np.array(stopys, dtype=np.int32),
+        np.array(jphi, dtype=np.float32),
+    )
+
+    event.Jet_pt = jet_pt
+    event.Jet_eta = jet_eta
+    event.fixedGridRhoFastjetAll = rho
+
+    event.Jet_genJetIdx = jet_genjetidx
+    event.GenJet_pt = genjet_pt
+    event.MET_pt = met_pt
+    event.MET_phi = met_phi
+    event.Jet_phi = jet_phi
+
+    module.begin(event)
+    module.event(event)
+
+    assert np.allclose(
+        event.Jet_ptResolution.content,
+        awk.JaggedArray(
+            np.array(starts, dtype=np.int32),
+            np.array(stopys, dtype=np.int32),
+            np.array(jptres, dtype=np.float32),
+        ).content,
+        rtol=1e-6, equal_nan=True,
+    )
+
+    assert np.allclose(
+        event.Jet_JECjerSF.content,
+        awk.JaggedArray(
+            np.array(starts, dtype=np.int32),
+            np.array(stopys, dtype=np.int32),
+            np.array(jjersf, dtype=np.float32),
+        ).content,
+        rtol=1e-6, equal_nan=True,
+    )
+
+    assert np.allclose(
+        event.Jet_JECjerSFDown.content,
+        awk.JaggedArray(
+            np.array(starts, dtype=np.int32),
+            np.array(stopys, dtype=np.int32),
+            np.array(jjersfdown, dtype=np.float32),
+        ).content,
+        rtol=1e-5, equal_nan=True,
+    )
+
+    assert np.allclose(
+        event.Jet_JECjerSFUp.content,
+        awk.JaggedArray(
+            np.array(starts, dtype=np.int32),
+            np.array(stopys, dtype=np.int32),
+            np.array(jjersfup, dtype=np.float32),
+        ).content,
+        rtol=1e-5, equal_nan=True,
+    )
+
+    assert np.allclose(
+        event.Jet_pt.content,
+        awk.JaggedArray(
+            np.array(starts, dtype=np.int32),
+            np.array(stopys, dtype=np.int32),
+            np.array(np.array(jpt)*np.array(jjersf), dtype=np.float32),
+        ).content,
+        rtol=1e-6, equal_nan=True,
+    )
+
+    assert np.allclose(
+        event.MET_pt,
+        np.array(new_met, dtype=np.float32),
+        rtol=1e-6, equal_nan=True,
+    )
+
+    assert np.allclose(
+        event.MET_phi,
+        np.array(new_mephi, dtype=np.float32),
+        rtol=1e-6, equal_nan=True,
+    )
