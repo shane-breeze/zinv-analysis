ee9960fdb281656c6fb5501d5e1a2517c9b4f7ef
Shane Breeze <s.breeze15@imperial.ac.uk>
2019-03-26 15:00:53 +0000

    Test WeightBTagging

--- tests/unit/sequence/Readers/test_WeightBTagging.py
+++ tests/unit/sequence/Readers/test_WeightBTagging.py
@@ -59,14 +59,44 @@ def test_weightbtagging_end(module):
 @pytest.mark.parametrize(
     "inputs,outputs", (
         [{
-            "jflav": [[0], [1, 2]],
-            "jeta": [[-2.3], [0.5, 3.1]],
-            "jptshift": [[20.], [2000., 100.]],
+            "jflav":    [[0]],
+            "jeta":     [[0.1]],
+            "jptshift": [[100.]],
+            "nsig":     0.,
+            "source":   "",
         }, {
+            "jbtagsf": [[1.094952666]],
+        }], [{
+            "jflav":    [[5], [-4, 0]],
+            "jeta":     [[-2.3], [0.5, 3.1]],
+            "jptshift": [[20.], [2000., 100.]],
+            "nsig":     0.,
+            "source":   "",
+        }, {
+            "jbtagsf": [[0.899436090086131], [0.992118451276882, 1.]]
+        }], [{
+            "jflav":    [[5], [-4, 0]],
+            "jeta":     [[-2.3], [0.5, 3.1]],
+            "jptshift": [[20.], [2000., 100.]],
+            "nsig":     1.,
+            "source":   "btagSF",
+        }, {
+            "jbtagsf": [[0.939649589304357], [1.186381134668450, 1.]]
+        }], [{
+            "jflav":    [[5], [-4, 0]],
+            "jeta":     [[-2.3], [0.5, 3.1]],
+            "jptshift": [[20.], [2000., 100.]],
+            "nsig":     -1.,
+            "source":   "btagSF",
+        }, {
+            "jbtagsf": [[0.859222590867906], [0.797855767885311, 1.]]
         }],
     )
 )
 def test_weightbtagging_begin(module, event, inputs, outputs):
+    event.nsig = inputs["nsig"]
+    event.source = inputs["source"]
+
     jflav = awk.JaggedArray.fromiter(inputs["jflav"]).astype(np.float32)
     jeta = awk.JaggedArray.fromiter(inputs["jeta"]).astype(np.float32)
     jptshift = awk.JaggedArray.fromiter(inputs["jptshift"]).astype(np.float32)
@@ -78,5 +108,15 @@ def test_weightbtagging_begin(module, event, inputs, outputs):
     event.Jet_ptShift = mock.Mock(side_effect=lambda ev: jptshift)
 
     assert module.begin(event) is None
-    print(event.Jet_btagSF(event))
-    #assert False
+
+    jbtagsf = event.Jet_btagSF(event)
+    ojbtagsf = awk.JaggedArray.fromiter(outputs["jbtagsf"]).astype(np.float32)
+    assert np.array_equal(jbtagsf.starts, ojbtagsf.starts)
+    assert np.array_equal(jbtagsf.stops, ojbtagsf.stops)
+    assert np.allclose(
+        jbtagsf.content, ojbtagsf.content, rtol=1e-6, equal_nan=True,
+    )
+
+def test_weightbtagging_end(module):
+    assert module.end() is None
+    assert module.calibrations is None
