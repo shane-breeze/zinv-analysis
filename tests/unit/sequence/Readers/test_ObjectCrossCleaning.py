7968b95cab2a7d3a94341988461b63c5b3d3cb67
Shane Breeze <s.breeze15@imperial.ac.uk>
2019-03-31 11:14:00 +0100

    update ObjectCrossCleaning tests

--- tests/unit/sequence/Readers/test_ObjectCrossCleaning.py
+++ tests/unit/sequence/Readers/test_ObjectCrossCleaning.py
@@ -14,7 +14,7 @@ class DummyEvent(object):
         self.iblock = 0
         self.nsig = 0
         self.source = ''
-        self.attribute_variation_sources = []
+        self.attribute_variation_sources = ["variation"]
 
         self.C1 = DummyColl()
         self.RC1 = DummyColl()
@@ -42,46 +42,69 @@ def test_objxclean_begin(module, event):
     assert module.begin(event) is None
     assert hasattr(event, "C1_XCleanMask")
 
-def test_objxclean_attr(module, event):
-    c1_starts = [0, 1, 3]
-    c1_stopys = [1, 3, 6]
-    event.C1.eta = awk.JaggedArray(
-        c1_starts, c1_stopys, np.array([0.3, 0.9, 1.2, 0.6 ,1.5, 1.8], dtype=np.float32),
+@pytest.mark.parametrize(
+    "inputs,outputs", (
+        [{
+            "c1_eta": [[0.3], [0.9, 1.2], [0.6, 1.5, 1.8]],
+            "c1_phi": [[0.15], [0.75, 1.05], [0.45, 1.35, 1.65]],
+            "rc1_eta": [[], [1.1], [2.5]],
+            "rc1_phi": [[], [0.78], [1.65]],
+            "rc2_eta": [[], [], [1.45]],
+            "rc2_phi": [[], [], [1.38]],
+            "source": "",
+            "nsig": 0.,
+        }, {
+            "xclean": [[True], [False, False], [True, False, True]],
+        }], [{
+            "c1_eta": [[0.3], [0.9, 1.2], [0.6, 1.5, 1.8]],
+            "c1_phi": [[0.15], [0.75, 1.05], [0.45, 1.35, 1.65]],
+            "rc1_eta": [[], [1.1], [2.5]],
+            "rc1_phi": [[], [0.78], [1.65]],
+            "rc2_eta": [[], [], [1.45]],
+            "rc2_phi": [[], [], [1.38]],
+            "source": "variation",
+            "nsig": 1.,
+        }, {
+            "xclean": [[True], [False, False], [True, False, True]],
+        }],
     )
-    event.C1.phi = awk.JaggedArray(
-        c1_starts, c1_stopys, np.array([0.15, 0.75, 1.05, 0.45, 1.35, 1.65], dtype=np.float32),
-    )
+)
+def test_objxclean_attr(module, event, inputs, outputs):
+    event.C1.eta = awk.JaggedArray.fromiter(inputs["c1_eta"]).astype(np.float32)
+    event.C1.phi = awk.JaggedArray.fromiter(inputs["c1_phi"]).astype(np.float32)
 
-    rc1_starts = [0, 0, 1]
-    rc1_stopys = [0, 1, 2]
     def rc1_call(ev, attr):
         if attr == "eta":
-            content = np.array([1.1, 2.5], dtype=np.float32)
+            return awk.JaggedArray.fromiter(
+                inputs["rc1_eta"],
+            ).astype(np.float32)
         elif attr == "phi":
-            content = np.array([0.78, 1.65], dtype=np.float32)
+            return awk.JaggedArray.fromiter(
+                inputs["rc1_phi"],
+            ).astype(np.float32)
         else:
+            print(attr)
             assert False
-        return awk.JaggedArray(rc1_starts, rc1_stopys, content)
     event.RC1 = mock.Mock(side_effect=rc1_call)
 
-    rc2_starts = [0, 0, 0]
-    rc2_stopys = [0, 0, 1]
     def rc2_call(ev, attr):
         if attr == "eta":
-            content = np.array([1.45], dtype=np.float32)
+            return awk.JaggedArray.fromiter(
+                inputs["rc2_eta"],
+            ).astype(np.float32)
         elif attr == "phi":
-            content = np.array([1.38], dtype=np.float32)
+            return awk.JaggedArray.fromiter(
+                inputs["rc2_phi"],
+            ).astype(np.float32)
         else:
+            print(attr)
             assert False
-        return awk.JaggedArray(rc2_starts, rc2_stopys, content)
     event.RC2 = mock.Mock(side_effect=rc2_call)
 
     module.begin(event)
     xclean = event.C1_XCleanMask(event)
+    oxclean = awk.JaggedArray.fromiter(outputs["xclean"]).astype(np.bool8)
 
-    assert np.array_equal(xclean.starts, np.array(c1_starts))
-    assert np.array_equal(xclean.stops, np.array(c1_stopys))
-    assert np.array_equal(
-        xclean.content,
-        np.array([True, False, False, True, False, True]),
-    )
+    assert np.array_equal(xclean.starts, oxclean.starts)
+    assert np.array_equal(xclean.stops, oxclean.stops)
+    assert np.array_equal(xclean.content, oxclean.content)
