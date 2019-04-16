06ddd94fc1dd812c200dd5e9fe9ccc9e48d2e6ef
Shane Breeze <s.breeze15@imperial.ac.uk>
2019-04-16 13:33:04 +0100

    Handling weights differently

--- zinv/sequence/Readers/WeightProducer.py
+++ zinv/sequence/Readers/WeightProducer.py
@@ -33,6 +33,7 @@ class WeightProducer(object):
         wvars = input_dict["weight_variations"]
         wvars = expand_pdf(wvars)
         avars = input_dict["attribute_variations"]
+        self.nuisances = None
         if self.nuisances is not None:
             self.nuisances = expand_pdf(self.nuisances)
             self.weight_variation_sources = [
@@ -44,28 +45,28 @@ class WeightProducer(object):
         else:
             self.weight_variation_sources = wvars
             self.attribute_variation_sources = avars
-        data_or_mc = "Data" if event.config.dataset.isdata else "MC"
+        #data_or_mc = "Data" if event.config.dataset.isdata else "MC"
 
-        self.lambda_functions = {
-            f: Lambda(f)
-            for _, labeldicts in weights_dict.items()
-            for _, regiondicts in labeldicts.items()
-            for f in regiondicts["Data"]
-        }
-        self.lambda_functions.update({
-            f: Lambda(f)
-            for _, labeldicts in weights_dict.items()
-            for _, regiondicts in labeldicts.items()
-            for f in regiondicts["MC"]
-        })
+        #self.lambda_functions = {
+        #    f: Lambda(f)
+        #    for _, labeldicts in weights_dict.items()
+        #    for _, regiondicts in labeldicts.items()
+        #    for f in regiondicts["Data"]
+        #}
+        #self.lambda_functions.update({
+        #    f: Lambda(f)
+        #    for _, labeldicts in weights_dict.items()
+        #    for _, regiondicts in labeldicts.items()
+        #    for f in regiondicts["MC"]
+        #})
 
-        for dataset, labeldicts in weights_dict.items():
-            for label, regiondicts in labeldicts.items():
-                weights = [self.lambda_functions[s] for s in regiondicts[data_or_mc]]
-                weighter = evaluate_weights("_".join([dataset, label, data_or_mc]), weights)
+        #for dataset, labeldicts in weights_dict.items():
+        #    for label, regiondicts in labeldicts.items():
+        #        weights = [self.lambda_functions[s] for s in regiondicts[data_or_mc]]
+        #        weighter = evaluate_weights("_".join([dataset, label, data_or_mc]), weights)
 
-                for region in regiondicts["Regions"]:
-                    setattr(event, "Weight_{}_{}_{}".format(dataset, region, data_or_mc), weighter)
+        #        for region in regiondicts["Regions"]:
+        #            setattr(event, "Weight_{}_{}_{}".format(dataset, region, data_or_mc), weighter)
 
         event.weight_variation_sources = self.weight_variation_sources
         event.attribute_variation_sources = self.attribute_variation_sources
@@ -74,5 +75,5 @@ class WeightProducer(object):
         event.weight_variation_sources = self.weight_variation_sources
         event.attribute_variation_sources = self.attribute_variation_sources
 
-    def end(self):
-        self.lambda_functions = None
+    #def end(self):
+    #    self.lambda_functions = None
