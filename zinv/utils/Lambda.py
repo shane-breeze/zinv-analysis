f847dfc09253ab95890c6f9652369d24efbb7778
Shane Breeze <s.breeze15@imperial.ac.uk>
2019-04-16 13:34:01 +0100

    Add get attr for min ref functino

--- zinv/utils/Lambda.py
+++ zinv/utils/Lambda.py
@@ -2,7 +2,7 @@ import awkward as awk
 import numpy as np
 np.warnings.filterwarnings('ignore')
 
-from AwkwardOps import get_nth_object, jagged_prod
+from AwkwardOps import get_nth_object, get_attr_for_min_ref, jagged_prod
 
 class Lambda(object):
     def __init__(self, function):
