b31bebb7fd04323d7662720916cf993867439b19
Shane Breeze <s.breeze15@imperial.ac.uk>
2019-04-17 13:38:42 +0100

    Fix WeightPdfScale tests

--- tests/unit/sequence/Readers/test_WeightPdfScale.py
+++ tests/unit/sequence/Readers/test_WeightPdfScale.py
@@ -22,6 +22,9 @@ class DummyEvent(object):
     def delete_branches(self, branches):
         pass
 
+    def hasbranch(self, branch):
+        return True
+
 @pytest.fixture()
 def event():
     return DummyEvent()
@@ -37,27 +40,53 @@ def module():
         [{
             "parent": "ZJetsToNuNu",
             "origpdf": [0., 1.02, 1.01],
-            "pdf": [[], [1.01, 1.02, 1.03, 1.04, 1.05], [0.99, 1.01, 0.98, 1.02, 1.1]],
-            "scale": [[], [1.01, 1.02, 1.03, 1.04, 1.05], [0.99, 1.01, 0.98, 1.02, 1.1]],
+            "npdf": 5,
+            "pdf": [
+                [0.65, 0.94, 0.91, 1.90, 0.50],
+                [1.01, 1.02, 1.03, 1.04, 1.05],
+                [0.99, 1.01, 0.98, 1.02, 1.1]
+            ],
+            "nscale": 9,
+            "scale": [
+                [1.01, 1.02, 1.03, 1.04, 1.05, 1.06, 1.07, 1.08, 1.09],
+                [0.91, 1.01, 0.92, 1.02, 0.93, 1.03, 0.94, 1.04, 0.95],
+                [0.99, 1.01, 0.98, 1.02, 0.97, 1.03, 0.96, 1.04, 0.95],
+            ],
             "source": "",
             "nsig": 0.,
         }, {
-            "pdf": [1., 1., 1.],
-            "scale": [1., 1., 1.],
+            "pdf": [0.91, 1.03, 0.98],
+            "scale": [1.04, 1.02, 1.02],
         }], [{
             "parent": "ZJetsToNuNu",
             "origpdf": [0., 1.02, 1.01],
-            "pdf": [[0.95, 0.96], [1.01, 1.02, 1.03, 1.04, 1.05], [0.99, 1.01, 0.98, 1.02, 1.1]],
-            "scale": [[], [1.01, 1.02, 1.03, 1.04, 1.05], [0.99, 1.01, 0.98, 1.02, 1.1]],
+            "npdf": 5,
+            "pdf": [
+                [0.65, 0.94, 0.91, 1.90, 0.50],
+                [1.01, 1.02, 1.03, 1.04, 1.05],
+                [0.99, 1.01, 0.98, 1.02, 1.1]
+            ],
+            "nscale": 9,
+            "scale": [
+                [1.01, 1.02, 1.03, 1.04, 1.05, 1.06, 1.07, 1.08, 1.09],
+                [0.91, 1.01, 0.92, 1.02, 0.93, 1.03, 0.94, 1.04, 0.95],
+                [0.99, 1.01, 0.98, 1.02, 0.97, 1.03, 0.96, 1.04, 0.95],
+            ],
             "source": "pdf1",
             "nsig": 1.,
         }, {
-            "pdf": [0.96, 1.02, 1.01],
-            "scale": [1., 1., 1.],
+            "pdf": [0.91, 1.03, 0.98],
+            "scale": [1.04, 1.02, 1.02],
         }], [{
             "parent": "ZJetsToNuNu",
             "origpdf": [0., 1.02, 1.01],
-            "pdf": [[], [1.01, 1.02, 1.03, 1.04, 1.05], [0.99, 1.01, 0.98, 1.02, 1.1]],
+            "npdf": 5,
+            "pdf": [
+                [0.65, 0.94, 0.91, 1.90, 0.50],
+                [1.01, 1.02, 1.03, 1.04, 1.05],
+                [0.99, 1.01, 0.98, 1.02, 1.1]
+            ],
+            "nscale": 9,
             "scale": [
                 [1.01, 1.02, 1.03, 1.04, 1.05, 1.06, 1.07, 1.08, 1.09],
                 [0.91, 1.01, 0.92, 1.02, 0.93, 1.03, 0.94, 1.04, 0.95],
@@ -66,12 +95,18 @@ def module():
             "source": "muf_scale",
             "nsig": -1.,
         }, {
-            "pdf": [1., 1., 1.],
+            "pdf": [0.91, 1.03, 0.98],
             "scale": [1.04, 1.02, 1.02],
         }], [{
             "parent": "QCD",
             "origpdf": [0., 1.02, 1.01],
-            "pdf": [[], [1.01, 1.02, 1.03, 1.04, 1.05], [0.99, 1.01, 0.98, 1.02, 1.1]],
+            "npdf": 5,
+            "pdf": [
+                [0.65, 0.94, 0.91, 1.90, 0.50],
+                [1.01, 1.02, 1.03, 1.04, 1.05],
+                [0.99, 1.01, 0.98, 1.02, 1.1]
+            ],
+            "nscale": 9,
             "scale": [
                 [1.01, 1.02, 1.03, 1.04, 1.05, 1.06, 1.07, 1.08, 1.09],
                 [0.91, 1.01, 0.92, 1.02, 0.93, 1.03, 0.94, 1.04, 0.95],
@@ -80,18 +115,28 @@ def module():
             "source": "muf_scale",
             "nsig": 1.,
         }, {
-            "pdf": [1., 1., 1.],
-            "scale": [1., 1., 1.],
+            "pdf": [0.91, 1.03, 0.98],
+            "scale": [1.04, 1.02, 1.02],
         }], [{
             "parent": "SingleTop",
             "origpdf": [0., 1.02, 1.01],
-            "pdf": [[], [1.01, 1.02, 1.03, 1.04, 1.05], [0.99, 1.01, 0.98, 1.02, 1.1]],
-            "scale": [[], [1.01, 1.02, 1.03, 1.04, 1.05], [0.99, 1.01, 0.98, 1.02, 1.1]],
+            "npdf": 5,
+            "pdf": [
+                [0.65, 0.94, 0.91, 1.90, 0.50],
+                [1.01, 1.02, 1.03, 1.04, 1.05],
+                [0.99, 1.01, 0.98, 1.02, 1.1]
+            ],
+            "nscale": 9,
+            "scale": [
+                [1.01, 1.02, 1.03, 1.04, 1.05, 1.06, 1.07, 1.08, 1.09],
+                [0.91, 1.01, 0.92, 1.02, 0.93, 1.03, 0.94, 1.04, 0.95],
+                [0.99, 1.01, 0.98, 1.02, 0.97, 1.03, 0.96, 1.04, 0.95],
+            ],
             "source": "pdf2",
             "nsig": -1.,
         }, {
-            "pdf": [1., 1., 1.],
-            "scale": [1., 1., 1.],
+            "pdf": [0.91, 1.03, 0.98],
+            "scale": [1.04, 1.02, 1.02],
         }],
     )
 )
@@ -108,13 +153,21 @@ def test_weightpdfscale(module, event, inputs, outputs):
     iscale = awk.JaggedArray.fromiter(inputs["scale"]).astype(np.float32)
 
     event.LHEWeight_originalXWGTUP = iorigpdf
+    event.nLHEPdfWeight = inputs["npdf"]
+    event.nLHEScaleWeight = inputs["nscale"]
     event.LHEPdfWeight = ipdf
     event.LHEScaleWeight = iscale
 
-    pdf = event.WeightPdfVariations(event)
-    scale = event.WeightQCDScale(event)
+    pdf = event.WeightPdfVariations(event, 2)
+    scale = event.WeightScaleVariations(event, 3)
     opdf = np.array(outputs["pdf"], dtype=np.float32)
     oscale = np.array(outputs["scale"], dtype=np.float32)
+
+    print(pdf)
+    print(opdf)
+
+    print(scale)
+    print(oscale)
 
     assert np.allclose(pdf, opdf, rtol=1e-6, equal_nan=True)
     assert np.allclose(scale, oscale, rtol=1e-6, equal_nan=True)
