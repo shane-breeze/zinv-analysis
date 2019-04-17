2f6301aa6df0f61f336ca8400037ed28f64d31b5
Shane Breeze <s.breeze15@imperial.ac.uk>
2019-04-17 13:43:24 +0100

    Update WeightProducer tests

--- tests/unit/sequence/Readers/test_WeightProducer.py
+++ tests/unit/sequence/Readers/test_WeightProducer.py
@@ -78,8 +78,8 @@ attribute_variations:
             "isdata": True,
             "nuisances": ["metTrigStat", "jesTotal"],
         }, {
-            "wvars": ["metTrigStat"],
-            "avars": ["jesTotal"],
+            "wvars": ["metTrigStat", "muonIdTightSF"],
+            "avars": ["jesTotal", "eleEnergyScale"],
         }],
     )
 )
@@ -98,81 +98,6 @@ def test_weightproducer_begin(module, event, inputs, outputs):
 @pytest.mark.parametrize(
     "inputs,outputs", (
         [{
-#            "isdata": True,
-#            "wtuple": [
-#                ("MET",            "Monojet",           "Data"),
-#                ("MET",            "SingleMuon",        "Data"),
-#                ("MET",            "SingleMuonQCD",     "Data"),
-#                ("SingleElectron", "SingleElectron",    "Data"),
-#                ("SingleElectron", "SingleElectronQCD", "Data"),
-#            ],
-#            "evattrs": [
-#                ("IsMETTriggered",            [True, True,  False]),
-#                ("IsSingleElectronTriggered", [True, False, False]),
-#            ],
-#            "nuisances": None,
-#        }, {
-#            "weights": [
-#                ("MET_Monojet_Data",                      [0., 0., 0.]),
-#                ("MET_SingleMuon_Data",                   [1., 1., 0.]),
-#                ("MET_SingleMuonQCD_Data",                [1., 1., 0.]),
-#                ("SingleElectron_SingleElectron_Data",    [1., 0., 0.]),
-#                ("SingleElectron_SingleElectronQCD_Data", [1., 0., 0.]),
-#            ]
-#        }], [{
-            "isdata": False,
-            "wtuple": [
-                ("MET",            "Monojet",           "MC"),
-                ("MET",            "SingleMuon",        "MC"),
-                ("MET",            "SingleMuonQCD",     "MC"),
-                ("SingleElectron", "SingleElectron",    "MC"),
-                ("SingleElectron", "SingleElectronQCD", "MC"),
-            ],
-            "evattrs": [
-                ("MonojetMC_Weights",         [0.3, 0.6, 0.9]),
-                ("SingleMuonMC_Weights",      [1.5, 1.1, 0.5]),
-                ("SingleElectronMC1_Weights", [0.9, 0.4, 1.2]),
-                ("SingleElectronMC2_Weights", [0.1, 0.3, 0.5]),
-                ("SingleElectronMC3_Weights", [2.4, 3.2, 0.1]),
-            ],
-            "nuisances": None,
-        }, {
-            "weights": [
-                ("MET_Monojet_MC",                      [0.3,   0.6,   0.9]),
-                ("MET_SingleMuon_MC",                   [1.5,   1.1,   0.5]),
-                ("MET_SingleMuonQCD_MC",                [1.5,   1.1,   0.5]),
-                ("SingleElectron_SingleElectron_MC",    [0.216, 0.384, 0.06]),
-                ("SingleElectron_SingleElectronQCD_MC", [0.216, 0.384, 0.06]),
-            ]
-        }],
-    )
-)
-def test_weightproducer_weights(module, event, inputs, outputs):
-    event.size = len(inputs["evattrs"][0][1])
-    event.config.dataset.isdata = inputs["isdata"]
-    module.nuisances = inputs["nuisances"]
-
-    for attr, arr in inputs["evattrs"]:
-        dtype = np.float32
-        if inputs["isdata"]:
-            dtype = np.bool8
-        setattr(event, attr, np.array(arr, dtype=dtype))
-
-    mocked_open = mock.mock_open(read_data=data)
-    with mock.patch("__builtin__.open", mocked_open):
-        module.begin(event)
-
-    for label, arr in outputs["weights"]:
-        win = getattr(event, "Weight_{}".format(label))(event)
-        wout = np.array(arr, dtype=np.float32)
-        print(label)
-        print(win)
-        print(wout)
-        assert np.allclose(win, wout, rtol=1e-6, equal_nan=True)
-
-@pytest.mark.parametrize(
-    "inputs,outputs", (
-        [{
             "isdata": True,
             "nuisances": None,
         }, {
@@ -182,8 +107,8 @@ def test_weightproducer_weights(module, event, inputs, outputs):
             "isdata": True,
             "nuisances": ["metTrigStat", "jesTotal"],
         }, {
-            "wvars": ["metTrigStat"],
-            "avars": ["jesTotal"],
+            "wvars": ["metTrigStat", "muonIdTightSF"],
+            "avars": ["jesTotal", "eleEnergyScale"],
         }],
     )
 )
@@ -200,7 +125,3 @@ def test_weightproducer_event(module, event, inputs, outputs):
 
     assert event.weight_variation_sources == outputs["wvars"]
     assert event.attribute_variation_sources == outputs["avars"]
-
-def test_weightproducer_end(module):
-    assert module.end() is None
-    assert module.lambda_functions is None
--- zinv/sequence/Readers/WeightProducer.py
+++ zinv/sequence/Readers/WeightProducer.py
@@ -15,12 +15,6 @@ def expand_pdf(nuisance_list):
             nuisance_list.append("pdf{}".format(i))
     return nuisance_list
 
-def evaluate_weights(name, weights):
-    @cachedmethod(operator.attrgetter('cache'), key=partial(hashkey, 'fevaluate_weights'))
-    def fevaluate_weights(ev, evidx, nsig, source, name_):
-        return reduce(operator.mul, weights)(ev)
-    return lambda ev: fevaluate_weights(ev, ev.iblock, ev.nsig, ev.source, name)
-
 class WeightProducer(object):
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
@@ -45,28 +39,6 @@ class WeightProducer(object):
         else:
             self.weight_variation_sources = wvars
             self.attribute_variation_sources = avars
-        #data_or_mc = "Data" if event.config.dataset.isdata else "MC"
-
-        #self.lambda_functions = {
-        #    f: Lambda(f)
-        #    for _, labeldicts in weights_dict.items()
-        #    for _, regiondicts in labeldicts.items()
-        #    for f in regiondicts["Data"]
-        #}
-        #self.lambda_functions.update({
-        #    f: Lambda(f)
-        #    for _, labeldicts in weights_dict.items()
-        #    for _, regiondicts in labeldicts.items()
-        #    for f in regiondicts["MC"]
-        #})
-
-        #for dataset, labeldicts in weights_dict.items():
-        #    for label, regiondicts in labeldicts.items():
-        #        weights = [self.lambda_functions[s] for s in regiondicts[data_or_mc]]
-        #        weighter = evaluate_weights("_".join([dataset, label, data_or_mc]), weights)
-
-        #        for region in regiondicts["Regions"]:
-        #            setattr(event, "Weight_{}_{}_{}".format(dataset, region, data_or_mc), weighter)
 
         event.weight_variation_sources = self.weight_variation_sources
         event.attribute_variation_sources = self.attribute_variation_sources
@@ -74,6 +46,3 @@ class WeightProducer(object):
     def event(self, event):
         event.weight_variation_sources = self.weight_variation_sources
         event.attribute_variation_sources = self.attribute_variation_sources
-
-    #def end(self):
-    #    self.lambda_functions = None
