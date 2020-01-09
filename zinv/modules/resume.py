ac7b14becb49df0fdbb718b33c5845c7f2731d41
Shane Breeze <s.breeze15@imperial.ac.uk>
2020-01-09 14:14:02 +0000

    Don't return files on resume

--- zinv/modules/resume.py
+++ zinv/modules/resume.py
@@ -2,8 +2,10 @@ import pysge
 
 def resume(
     path, batch_opts="-q hep.q", sleep=5, request_resubmission_options=True,
+    return_files=False,
 ):
     return pysge.sge_resume(
         "zinv", path, options=batch_opts, sleep=sleep,
         request_resubmission_options=request_resubmission_options,
+        return_files=return_files,
     )
